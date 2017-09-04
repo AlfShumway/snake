0bc502deba27089ddb974d8ebe56ff0a8a06d4a4
AlfShumway <amami.riku.96@gmail.com>
2017-09-04 21:05:24 +0500

    Создание класса список

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -14,8 +14,16 @@ namespace snake
 
         public Point()
         {
-            Console.WriteLine("Создается новая точка");
+            
         }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        
 
         public void Draw()
         {
