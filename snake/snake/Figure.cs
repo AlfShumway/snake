59c915946a69f39e1a70004bfb6857884cf23d15
AlfShumway <amami.riku.96@gmail.com>
2017-09-07 19:56:59 +0500

    Абстракция

--- snake/snake/Figure.cs
+++ snake/snake/Figure.cs
@@ -10,7 +10,6 @@ namespace snake
     {
        protected List<Point> pList;
 
-
         public void Draw()
         {
             foreach (Point p in pList)
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -14,12 +14,6 @@ namespace snake
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             upLine.Draw();
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
@@ -29,6 +23,9 @@ namespace snake
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             rightLine.Draw();
 
+            Point p = new Point(4, 5, '#');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
         }      
