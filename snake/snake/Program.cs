9c1ac400fc4bd9160fa30b0adb5980072b283c5a
AlfShumway <amami.riku.96@gmail.com>
2017-09-08 10:22:58 +0500

    Движение

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace snake
 {
@@ -23,10 +24,28 @@ namespace snake
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             rightLine.Draw();
 
+            // Отрисовка точек
             Point p = new Point(4, 5, '#');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+            snake.Move();
 
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+           
             Console.ReadLine();
         }      
     }
